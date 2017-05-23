91ac290ece3a038cd87d7dc000794bc6f2b9796f
konthailuktung <konthailuktung@gmail.com>
2017-05-23 10:04:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php
-echo "Hello LINE BOT";
-
-echo "juub";
-
-?>
+<div style="margin: 0 auto; width:100%; height:400px;">
+    <object type="text/html" data="http://webla1000.blogspot.com/"
+            style="width:100%; height:100%; margin:1%;">
+    </object>
+</div>
